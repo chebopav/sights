c8885583d842bf416e69b4a46bb96e971a705a25
user <chebopav@bk.ru>
2021-01-17 20:54:04 +0300

    add comments

---
+++ sights/src/main/java/com/project/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.project.repository;
+
+import com.project.entity.data.Comment;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends PagingAndSortingRepository<Comment, Long> {
+}
