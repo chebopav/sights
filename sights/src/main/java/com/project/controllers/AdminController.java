59bcaa7420374cf8800791acc25283d4888de4b7
WishSlayer <wishslayer@icloud.com>
2021-01-28 07:26:57 +0300

    123

---
+++ sights/src/main/java/com/project/controllers/AdminController.java
@@ -0,0 +1,22 @@
+package com.project.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    @Autowired
+    public AdminController() {
+    }
+
+    @GetMapping(value = "/show")
+    public String admin(Model model){
+        return "admin";
+    }
+}
+
